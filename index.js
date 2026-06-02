c19fbce62a0d726d74acda525d0ae0f97f5c2a79
Sherlock1181 <sherlockeesherlock@gmail.com>
2026-06-02 23:00:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildPresences
+    ] 
+});
 
 client.once('ready', () => {
     console.log('Bot is online!');
 });
 
-client.login('YOUR_BOT_TOKEN_HERE');
+client.login('MTQ0MDU4MDA2NjY4NDk2MDc4OA.GpPcsR.zNefgizQ3x3UX6mn88EmKDChxNunRffjeidn9c');
